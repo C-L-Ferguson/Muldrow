978ba12a8d6df80d2e08ddbeef36942d12c19861
C-L-Ferguson <carolineferguson@uchicago.edu>
2026-07-28 16:13:11 -0500

    Update Muldrow_Code.R

--- Muldrow_Code.R
+++ Muldrow_Code.R
@@ -4,3 +4,7 @@ install.packages("readxls")
 summary(muldrow_coding_sheet_v2)
 data_set <- muldrow_coding_sheet_v2
 dim(data_set)
+npm install -g @anthropic-ai/claude-code
+
+g @anthropic-ai/claude-code
+npm install -g @anthropic-ai/claude-code
